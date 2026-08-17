c943b613f7ffb920b7dc67199a6e8c78436e8a49
dantes <105533299+0xaDanteees@users.noreply.github.com>
2026-08-16 20:26:42 -0600

    feat: update CV download link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 export const EXTERNAL_LINKS = {
   github: 'https://github.com/0xaDanteees',
   linkedin: 'https://www.linkedin.com/in/daniel-r-ortega/',
-  cv: 'https://drive.google.com/uc?export=download&id=1f87CBKWscrqUY1t3T-nPEdgf0wM1Yjo2'
+  cv: 'https://drive.google.com/uc?export=download&id=1gyhbi0zrMo_S_rtRZCdNl1eqdRjrzMkj'
 } as const
 
 export const SOCIAL_HANDLES = {
